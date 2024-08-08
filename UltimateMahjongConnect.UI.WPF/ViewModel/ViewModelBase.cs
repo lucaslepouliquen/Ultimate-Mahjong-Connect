5752ed8fdb4dee87f02fed59553af6c701ca3f46
Lucas Le Pouliquen <lucaslepouliquen@gmail.com>
2024-08-08 13:43:09 +0200

    Add ViewModelBase class

---
+++ UltimateMahjongConnect.UI.WPF/ViewModel/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UltimateMahjongConnect.UI.WPF.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual Task LoadAsync() => Task.CompletedTask;
+    }
+}
