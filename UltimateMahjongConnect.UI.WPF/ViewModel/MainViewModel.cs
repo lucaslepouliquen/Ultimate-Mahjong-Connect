02c2c68052654cbac6c889f40effa87d22cc043b
Lucas Le Pouliquen <lucaslepouliquen@gmail.com>
2024-08-08 14:24:54 +0200

    Add MainViewModel and DelegateCommand

---
+++ UltimateMahjongConnect.UI.WPF/ViewModel/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+
+namespace UltimateMahjongConnect.UI.WPF.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase? _selectedViewModel;
+        public MainViewModel() 
+        {
+            SelectViewModelCommand = new DelegateCommand(SelectViewModel);
+        }
+
+        public ViewModelBase? SelectedViewModel
+        {
+            get => _selectedViewModel; 
+
+            set
+            {
+                _selectedViewModel = value;
+                RaisePropertyChanged();
+            }
+        }
+        public DelegateCommand SelectViewModelCommand { get; }
+        public async override Task LoadAsync()
+        {
+            if (SelectedViewModel is not null)
+            {
+                await SelectedViewModel.LoadAsync();
+            }
+        }
+
+        private async void SelectViewModel(object? parameter)
+        {
+            SelectedViewModel = parameter as ViewModelBase;
+            await LoadAsync();
+        }
+    }
+}
