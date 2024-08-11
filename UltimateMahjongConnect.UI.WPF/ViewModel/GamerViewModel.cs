87dc435db6becbc9b930d82ed69ce33b11e64dab
Lucas Le Pouliquen <lucaslepouliquen@gmail.com>
2024-08-12 00:00:51 +0200

    Add ValidationViewModelBase, GamerView, GamerViewModel

---
+++ UltimateMahjongConnect.UI.WPF/ViewModel/GamerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UltimateMahjongConnect.UI.WPF.ViewModel
+{
+    public class GamerViewModel : ValidationViewModelBase
+    {
+        
+    }
+}
