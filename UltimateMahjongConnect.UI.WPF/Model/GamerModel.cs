f7c646ca82e85831eab1ddeca5ba9f0e6c1368c1
Lucas Le Pouliquen <lucaslepouliquen@gmail.com>
2024-08-13 16:55:50 +0200

    Add GamerModel to WPF project

---
+++ UltimateMahjongConnect.UI.WPF/Model/GamerModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateMahjongConnect.UI.WPF.Model
+{
+    public class GamerModel
+    {
+        public string? Pseudonyme { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string? Password { get; set; }
+
+        public GamerModel()
+        {
+            Pseudonyme = "";
+            Age = 0;
+            Email = "";
+            Password = "";  
+        }
+
+        public GamerModel(string pseudonyme, int age, string email, string password)
+        {
+            Pseudonyme = pseudonyme;
+            Age = age;
+            Email = email;
+            Password = password;
+        }
+    }
+}
