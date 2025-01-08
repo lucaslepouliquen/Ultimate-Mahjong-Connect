45d13d02a605c870352ed80062c55ed834993281
LucasLePOULIQUEN <LucasLePOULIQUEN@IBM-PF4SDRSF>
2025-01-08 13:42:25 +0100

    Add duende identity server Provider

---
+++ Yukanotoko.IDP/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Yukanotoko.IDP.Models;
+
+namespace Yukanotoko.IDP.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
