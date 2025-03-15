68ec65c8d0b892d850e6a5fb24a232089b8d10a0
Lucas Le Pouliquen <lucaslepouliquen@outlook.fr>
2025-03-15 14:31:20 +0100

    Fix GamerController

--- UltimateMahjongConnect.WebApi/Program.cs
+++ UltimateMahjongConnect.WebApi/Program.cs
@@ -1,9 +1,14 @@
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Ultimate_Mahjong_Connect.Configurations;
+using UltimateMahjongConnect.Application.Interface;
+using UltimateMahjongConnect.Application.Profiles;
+using UltimateMahjongConnect.Application.Services;
 using UltimateMahjongConnect.Domain.Interfaces;
 using UltimateMahjongConnect.Domain.Models;
 using UltimateMahjongConnect.Infrastructure.Models;
+using UltimateMahjongConnect.Infrastructure.Profiles;
+using UltimateMahjongConnect.Infrastructure.Repositories;
 
 
 
@@ -12,7 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbSQLContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
     .EnableSensitiveDataLogging()
     .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
@@ -31,10 +36,25 @@ builder.Services.AddApiVersioningService().AddSwaggerGen().ConfigureOptions<Swag
 builder.Services.AddTransient<IApiVersionDescriptionProvider, DefaultApiVersionDescriptionProvider>();
 
 //Register Mahjong services
+builder.Services.AddAutoMapper(
+    typeof(Program).Assembly,
+    typeof(GamerDTOProfile).Assembly,             
+    typeof(GamerEntityProfile).Assembly
+    );
+builder.Services.AddTransient<IGamerRepository,GamerRepository>();
+builder.Services.AddTransient<GamerService>();
 builder.Services.AddTransient<IMahjongTile, MahjongTile>();
 builder.Services.AddTransient<IMahjongBoard, MahjongBoard>();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbSQLContext>();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -54,6 +74,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 app.UseAuthorization();
 
 app.MapControllers();
