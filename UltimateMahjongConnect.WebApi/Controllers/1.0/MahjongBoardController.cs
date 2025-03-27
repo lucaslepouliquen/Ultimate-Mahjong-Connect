bcf32abffcb9183ef184bf59381edf4fda0cd816
Lucas Le Pouliquen <lucaslepouliquen@outlook.fr>
2025-03-27 23:00:24 +0100

    Add modifications for MahjongBoardController

--- UltimateMahjongConnect.WebApi/Controllers/1.0/MahjongBoardController.cs
+++ UltimateMahjongConnect.WebApi/Controllers/1.0/MahjongBoardController.cs
@@ -16,25 +16,39 @@ namespace Ultimate_Mahjong_Connect.Controllers._1._0
 
         [AllowAnonymous]
         [HttpGet("board")]
-        public IActionResult InitializeBoardRandom([FromQuery] string mode= "deterministic")
+        public IActionResult InitializeBoardRandom([FromQuery] string mode = "deterministic")
         {
-            if(mode == "deterministic")
+            try
             {
-                _mahjongBoard.InitializeBoardDeterministically();
+                if (mode == "deterministic")
+                {
+                    _mahjongBoard.InitializeBoardDeterministically();
+                }
+                else
+                {
+                    _mahjongBoard.ReverseInitializeBoardDeterministically();
+                }
+                return Ok(_mahjongBoard.GetBoard());
             }
-            else 
+            catch (Exception ex)
             {
-                _mahjongBoard.ReverseInitializeBoardDeterministically();
+                return BadRequest(ex.Message);
+
             }
-            return Ok(_mahjongBoard.GetBoard());
         }
 
         [AllowAnonymous]
         [HttpGet("boards/path")]
         public IActionResult GetPath([FromQuery] int row1, [FromQuery] int column1, [FromQuery] int row2, [FromQuery] int column2)
         {
-            var mahjongPath = _mahjongBoard.GetValidatedPath(row1, column1, row2, column2);
-            return Ok(mahjongPath);
+            try {
+                var mahjongPath = _mahjongBoard.GetValidatedPath(row1, column1, row2, column2);
+                return Ok(mahjongPath);
+            } catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
         }
     }
 }
